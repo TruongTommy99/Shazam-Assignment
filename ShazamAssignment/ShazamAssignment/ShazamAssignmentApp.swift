bb5ba50ed396b3044ff0e89a160637596c69f84a
Truong Tommy <79810374+TruongTommy99@users.noreply.github.com>
2023-01-22 16:11:33 -0500

    Initial Commit
    Added source code

---
+++ ShazamAssignment/ShazamAssignment/ShazamAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamAssignmentApp.swift
+//  ShazamAssignment
+//
+//  Created by Tommy Truong on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ShazamAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
